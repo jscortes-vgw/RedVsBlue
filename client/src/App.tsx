5dd3bdb5afcdf3f38528a286c2f612a27e5594f4
Jonathan Chueh <jonathan.chueh@vgw.co>
2025-03-20 14:25:27 +1100

    add result modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import FloatingButtonWithMenu from "./pages/floating";
 
+import { Button } from "@mui/material";
+import { ResultModal } from "./pages/modals/result-modal";
 
 export const App: React.FC = () => {
+  const [playerTeam, setPlayerTeam] = useState("Blue");
+  const [isResultModalOpen, setIsResultModalOpen] = useState<boolean>(false);
+
   return (
     <div>
       <FloatingButtonWithMenu />
+      <Button onClick={handleOpenResultModal}>Icon</Button>
+      <ResultModal
+        open={isResultModalOpen}
+        handleClose={handleCloseResultModal}
+        currentTeam={playerTeam}
+      />
     </div>
   );
-}
+
+  function handleOpenResultModal() {
+    setIsResultModalOpen(true);
+  }
+
+  function handleCloseResultModal() {
+    setIsResultModalOpen(false);
+  }
+};
