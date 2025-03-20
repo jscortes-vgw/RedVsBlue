42ff30ecc9a99f273a084e4531908bfd4d95dc05
jscortes-vgw <juan.guzman@vgw.co>
2025-03-20 14:28:13 +1100

    fix index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,11 @@
 import React from "react";
+import FloatingButtonWithMenu from "./pages/floating";
 
-interface MyButtonProps {
-  /** The text to display inside the button */
-  title: string;
-  /** Whether the button can be interacted with */
-  disabled: boolean;
-}
-
-function MyButton({ title, disabled }: MyButtonProps) {
-  return (
-    <button disabled={disabled}>{title}</button>
-  );
-}
 
 export const App: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton title="I'm a disabled button" disabled={true}/>
+      <FloatingButtonWithMenu />
     </div>
   );
 }
